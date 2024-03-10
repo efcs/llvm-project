a57e4ac74b3a9bdbc4996b5312c2df1c1f8259f4
Eric Fiselier <eric@efcs.ca>
2024-03-10 03:52:33 -0400

    changes

--- libcxx/cmake/Modules/LibcxxStdModules.cmake
+++ libcxx/cmake/Modules/LibcxxStdModules.cmake
@@ -1,13 +1,30 @@
+set(INCLUDE_PATH "/tmp/shit/include/c++/v1/" )
+set(CONFIG_INCLUDE_PATH "/tmp/shit/include/x86_64-unknown-linux-gnu/c++/v1"))
+
+target_compile_options(std.compat
+  PUBLIC
+    -nostdinc++
+    -Wno-reserved-module-identifier
+    -Wno-reserved-user-defined-literal
+	-fmodule-file=std=${CMAKE_CURRENT_BINARY_DIR}/CMakeFiles/std.dir/std.pcm
+    @LIBCXX_COMPILE_FLAGS@
+)
+set_target_properties(std.compat
+  PROPERTIES
+    OUTPUT_NAME   "c++std.compat"
+)
+add_dependencies(std.compat std)
+
 if(NOT TARGET std)
     add_library(std INTERFACE)
     target_compile_features(std INTERFACE cxx_std_23)
     target_sources(std INTERFACE
         FILE_SET cxx_modules TYPE CXX_MODULES FILES
-            "${CMAKE_CURRENT_LIST_DIR}/std.cppm"
+            "${INCLUDE_PATH}/__modules/std.cppm"
     )
     target_include_directories(std SYSTEM INTERFACE
-        "${LIBCXX_GENERATED_INCLUDE_TARGET_DIR}"
-        "${LIBCXX_GENERATED_INCLUDE_DIR}"
+        ${INCLUDE_PATH}
+        ${CONFIG_INCLUDE_PATH}
     )
     target_compile_options(std INTERFACE
         -nostdinc++
@@ -24,11 +41,11 @@ if(NOT TARGET std.compat)
     target_compile_features(std.compat INTERFACE cxx_std_23)
     target_sources(std.compat INTERFACE
         FILE_SET cxx_modules TYPE CXX_MODULES FILES
-            "${CMAKE_CURRENT_LIST_DIR}/std.compat.cppm"
+            "${INCLUDE_PATH}/__modules/std.compat.cppm"
     )
-    target_include_directories(std.compat SYSTEM INTERFACE
-        "${LIBCXX_GENERATED_INCLUDE_TARGET_DIR}"
-        "${LIBCXX_GENERATED_INCLUDE_DIR}"
+    target_include_directories(std SYSTEM INTERFACE
+        ${INCLUDE_PATH}
+        ${CONFIG_INCLUDE_PATH}
     )
     target_compile_options(std.compat INTERFACE
         -nostdinc++
